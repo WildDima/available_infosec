1ca0af9da1cd886598c14ea19347944272022094
WildDima <dtopornin@gmail.com>
2016-05-22 17:24:10 +0300

    add index, fixes for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522084451) do
+ActiveRecord::Schema.define(version: 20160522142256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20160522084451) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "articles", ["title"], name: "index_articles_on_title", using: :btree
 
   create_table "images", force: :cascade do |t|
     t.string   "image"
